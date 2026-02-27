55557031cdf7fa65509e7cedace73a32c6059629
Benedek Feke <142662446+benedekfeke@users.noreply.github.com>
2026-02-27 14:10:19 +0100

    removed manual sync

--- app/components/HeaderClient.tsx
+++ app/components/HeaderClient.tsx
@@ -87,12 +87,12 @@ export default function HeaderClient({user}: {user: User | null}) {
             >
             TestPage
           </Link>
-          <Button
+          {/* <Button
             onClick={handleSync}
             className="z-10 group inline-flex h-9 w-max items-center justify-center rounded-lg bg-white/10 backdrop-blur-md border border-white/20 px-4 py-2 text-base text-gray-50 hover:bg-black hover:border-white transition-all duration-300 hover:rounded-none"
             >
             Sync with local DB
-          </Button>
+          </Button> */}
           {!user && (
             <Link
             href="/auth/login"
