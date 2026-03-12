cf98867e248e7efea9ccf59850464fd0e36ece58
Benedek Feke <142662446+benedekfeke@users.noreply.github.com>
2026-03-12 21:23:19 +0100

    update build, instructions

--- public/Build/webDemo.framework.js
+++ public/Build/webDemo.framework.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66f98fd4b1f480ec6fd0da26df7cccc6bb060c5256761d838755fd3b2d6a8a62
+oid sha256:bafe60c77dfb4a6ffeb0a35318d7add8a5708bc5919d08204ca6778d2d33d919
 size 702528
--- src/domain/algorithm/configs/trie.config.ts
+++ src/domain/algorithm/configs/trie.config.ts
@@ -3,7 +3,7 @@ import { AlgorithmConfig } from "../algorithm.types";
 export const trieConfig: AlgorithmConfig = {
   sceneName: 'Trie',
   title: 'Trie Data Structure',
-  instructions: 'Instructions: Find the target name, select the currently highlighted shelf/column/row/book to match the highlighted character on top. Move with WASD, select with "Space".',
+  instructions: 'Instructions: Imagine you are in a virtual library, your goal is to find an author\'s name that you saw somewhere, the trie data structure acts just like a librarian, but the trie is much more powerful.Sselect the currently highlighted letter(on top) by selecting the objects in order: shelf->column->row->book. Move with WASD, select with "Space".',
   description: `A trie (pronounced “try”) is a tree-based data structure that stores strings efficiently by sharing common prefixes. Also called a prefix tree, a trie enables fast string search, insertion, and deletion operations in O(L) time, where L is the string length.`,
   modalDescription: [
     { text: 'A ' },
